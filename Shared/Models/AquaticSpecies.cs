bcd149134e4ddf18048f516c752585e9f5efc508
Igor Velikic <ivelikic@gmail.com>
2026-06-19 13:52:30 +0200

    AquaticSpecies model

---
+++ Shared/Models/AquaticSpecies.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Shared.Models
+{
+    [DataContract]
+    public class AquaticSpecies
+    {
+        [DataMember]
+        public Guid Id { get; set; }
+
+        [DataMember]
+        public string Name { get; set; } = string.Empty;
+
+        [DataMember]
+        public string ScientificName { get; set; } = String.Empty;
+
+        [DataMember]
+        public string Habitat { get; set; } = String.Empty;
+
+        [DataMember]
+        public string WaterType { get; set; } = String.Empty;
+
+        [DataMember]
+        public int AverageLifespand { get; set; }
+    }
+}
