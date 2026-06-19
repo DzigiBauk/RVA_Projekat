b78782721709e39029c89c24c93d4bab479654a8
Bojana Glumac <bojanaglumac21@gmail.com>
2026-06-20 00:51:10 +0200

    WCF komunikacija - pribavljanje podataka

---
+++ Komponenta2/Services/AquariumClient.cs
@@ -0,0 +1,33 @@
+﻿using Komponenta2.Interfaces;
+using Shared.Contracts;
+using Shared.Models;
+
+namespace Komponenta2.Services
+{
+    public class AquariumClient : IAquariumClient
+    {
+        private readonly IAquariumService proxy;
+
+        public AquariumClient(IAquariumService proxy)
+        {
+            this.proxy = proxy;
+        }
+
+        public async Task<List<WaterQualityReading>> GetReadings(Guid speciesId, int month)
+        {
+            var request = new ReadingsRequest
+            {
+                SpeciesId = speciesId,
+                Month = month,
+                Year = DateTime.Now.Year
+            };
+
+            return await proxy.GetReadingsAsync(request);
+        }
+
+        public async Task<List<AquaticSpecies>> GetSpecies()
+        {
+            return await proxy.GetSpeciesAsync();
+        }   
+    }
+}
